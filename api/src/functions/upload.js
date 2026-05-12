8cf5551873c1875a89c4e7ea92053cd60af5c904
HIT H.Aso <209725340+aso-htm@users.noreply.github.com>
2026-05-12 14:41:22 +0900

    upload.js npm in

--- api/src/functions/upload.js
+++ api/src/functions/upload.js
@@ -1,9 +1,31 @@
 import { app } from '@azure/functions';
+import { BlobServiceClient } from "@azure/storage-blob";
 
 app.http('upload', {
     methods: ['GET'],
     authLevel: 'anonymous',
-    handler: async () => {
-        return { status: 200, body: "upload function is alive!" };
+    handler: async (request, context) => {
+        try {
+            const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+
+            if (!connectionString) {
+                return { status: 500, body: "Connection string not set" };
+            }
+
+            const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+            const containerClient = blobServiceClient.getContainerClient("sample");
+
+            const blobName = "hello-from-swa.txt";
+            const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+            const content = "Hello Blob from SWA!";
+            const data = Buffer.from(content, "utf-8");
+
+            await blockBlobClient.uploadData(data);
+
+            return { status: 200, body: `Uploaded: ${blobName}` };
+        } catch (err) {
+            return { status: 500, body: `Error: ${err.message}` };
+        }
     }
 });
